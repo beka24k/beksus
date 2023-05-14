1e4bf560a6f5ae2fb047d4d638aa554db8eb1b67
beka24k <110625229+beka24k@users.noreply.github.com>
2023-05-14 12:58:39 +0600

    main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+        MyHashtable aa=new MyHashtable();
+        MyTestingClass asd=new MyTestingClass(null,null);
+        asd.tenThousandtest();
+        asd.tester();
+        }
 }
